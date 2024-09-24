4937d8cb3c9ecde383c276f086e478ea98798c36
Lerian Febriana <kanglerian@gmail.com>
2024-09-24 16:36:01 +0700

    nama database

--- models/viewquestionusers.js
+++ models/viewquestionusers.js
@@ -23,7 +23,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'ViewQuestionUsers',
-    tableName: 'viewquestionusers',
+    tableName: 'ViewQuestionUsers',
     timestamps: false,
   });
   return ViewQuestionUsers;
