ab51f69f0e40fb8245e0f7795b650aa900cf95b9
Lerian Febriana <kanglerian@gmail.com>
2024-09-28 20:23:38 +0700

    config

--- config/config.js
+++ config/config.js
@@ -1,10 +1,20 @@
-{
+require('dotenv').config();
+const {
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_NAME,
+  DB_HOST,
+  DB_DIALECT,
+  DB_PORT
+} = process.env;
+module.exports = {
   "development": {
-    "username": "amisbud1_trisaksi",
-    "password": "Trisakti",
-    "database": "amisbud1_cat_db",
-    "host": "andria-db.id.domainesia.com",
-    "dialect": "mysql"
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": DB_NAME,
+    "host": DB_HOST,
+    "dialect": DB_DIALECT,
+    "port": DB_PORT
   },
   "test": {
     "username": "dev",
