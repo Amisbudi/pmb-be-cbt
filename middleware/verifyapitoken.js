51e60e269a88d9d10b5bf034406baa10c53b604c
Lerian Febriana <kanglerian@gmail.com>
2024-10-01 00:38:13 +0700

    api-key

--- middleware/verifyapitoken.js
+++ middleware/verifyapitoken.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const { API_SECRET } = process.env;
 
 module.exports = async (req, res, next) => {
-  const key = req.headers['lp3i-api-key'];
+  const key = req.headers['api-key'];
   if (!key || key !== API_SECRET) {
     return res.status(403).json({
       message: 'Invalid API Key'
