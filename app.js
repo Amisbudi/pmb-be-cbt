3e4cd050f917d47e4ffebc610f63e0413cdb0795
haisyamm <70573490+haisyamm@users.noreply.github.com>
2024-11-26 13:24:26 +0700

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const packagequestionusersRouter = require('./routes/packagequestionusers');
 
 const app = express();
 
-app.use(cors());
+app.use(cors(['https://be-cbt.trisakti.ac.id', 'https://admisi.trisakti.ac.id]));
 app.use(logger('dev'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
