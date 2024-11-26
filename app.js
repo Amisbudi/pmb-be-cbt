143d036fb91b93d3d110a2ebb15d7bf95d270411
haisyamm <70573490+haisyamm@users.noreply.github.com>
2024-11-26 14:23:09 +0700

    Update app.js

--- app.js
+++ app.js
@@ -16,24 +16,7 @@ const packagequestionusersRouter = require('./routes/packagequestionusers');
 
 const app = express();
 
-const allowedOrigins = [
-  'https://cbt.trisakti.ac.id',
-  'https://be-cbt.trisakti.ac.id',
-  'https://admisi.trisakti.ac.id',
-];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
